1d6cdf2651233480ad853e7538676b245b0bf6cf
fordchen <fordchen@tencent.com>
2021-02-26 17:06:52 +0800

    【高级】立方体贴图笔记

--- CLion CMake _Mac/resources/shaders/advancedGL/6.2.cubemaps.fs
+++ CLion CMake _Mac/resources/shaders/advancedGL/6.2.cubemaps.fs
@@ -19,3 +19,7 @@ void main()
     vec3 R = refract(I, normalize(Normal), ratio); // 计算折射光线
     FragColor = vec4(texture(skybox, R).rgb, 1.0);
 }
+
+// 注意，这样实现的反射、折射效果，无法反应环境中的其他物体，如果要实现真实的反射效果，
+// 一种办法是使用动态环境映射：即每次渲染立方体之前，先用FBO渲染六个方向，然后生成一个cubemap，然后渲染立方体时，基于该cubemap进行采样；
+// 二种方法是使用反射探针：参考https://github.com/tj41694/tinyEngine
