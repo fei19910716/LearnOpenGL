323d54f03440521f7d3d594dfe58a7877df7c14f
fordchen <fordchen@tencent.com>
2021-02-26 16:57:36 +0800

    【高级】利用立方体贴图实现折射效果

--- CLion CMake _Mac/resources/shaders/advancedGL/6.2.cubemaps.fs
+++ CLion CMake _Mac/resources/shaders/advancedGL/6.2.cubemaps.fs
@@ -9,7 +9,13 @@ uniform samplerCube skybox;
 
 void main()
 {
-    vec3 I = normalize(Position - cameraPos); // 计算camera到片元的向量
-    vec3 R = reflect(I, normalize(Normal)); // 计算反射向量
-    FragColor = vec4(texture(skybox, R).rgb, 1.0); // 用反射向量在cubemap上采样
+//     vec3 I = normalize(Position - cameraPos); // 计算camera到片元的向量
+//     vec3 R = reflect(I, normalize(Normal)); // 计算反射向量
+//     FragColor = vec4(texture(skybox, R).rgb, 1.0); // 用反射向量在cubemap上采样
+
+    //折射效果
+    float ratio = 1.00 / 1.52; // 空气到玻璃的折射率
+    vec3 I = normalize(Position - cameraPos);
+    vec3 R = refract(I, normalize(Normal), ratio); // 计算折射光线
+    FragColor = vec4(texture(skybox, R).rgb, 1.0);
 }
