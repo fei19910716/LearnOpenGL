24beef2b6b4e6ec43c003dbe53f82df5e5292250
fordchen <fordchen@tencent.com>
2021-03-17 11:56:40 +0800

    【高级光照】修复阴影失真产生的条纹黑线问题(Shadow Acne)

--- CLion CMake _Mac/resources/shaders/advanced_light/3.1.2.shadow_mapping.fs
+++ CLion CMake _Mac/resources/shaders/advanced_light/3.1.2.shadow_mapping.fs
@@ -14,7 +14,7 @@ uniform sampler2D shadowMap;
 uniform vec3 lightPos;
 uniform vec3 viewPos;
 
-float ShadowCalculation(vec4 fragPosLightSpace)
+float ShadowCalculation(vec4 fragPosLightSpace, float bias)
 {
     // perform perspective divide, result in [-1,1]
     vec3 projCoords = fragPosLightSpace.xyz / fragPosLightSpace.w;
@@ -25,7 +25,11 @@ float ShadowCalculation(vec4 fragPosLightSpace)
     // get depth of current fragment from light's perspective
     float currentDepth = projCoords.z;
     // check whether current frag pos is in shadow
-    float shadow = currentDepth > closestDepth  ? 1.0 : 0.0;
+    // float shadow = currentDepth > closestDepth  ? 1.0 : 0.0;
+
+    // 修复阴影失真产生的条纹黑线问题(Shadow Acne), 阴影失真主要是因为阴影贴图受限于分辨率，在距离光源比较远的情况下，多个片段可能从深度贴图的同一个值中去采样
+    // float bias = 0.005; // just use a constant bias
+    float shadow = currentDepth - bias > closestDepth  ? 1.0 : 0.0;
 
     return shadow;
 }
@@ -49,7 +53,8 @@ void main()
     spec = pow(max(dot(normal, halfwayDir), 0.0), 64.0);
     vec3 specular = spec * lightColor;
     // calculate shadow
-    float shadow = ShadowCalculation(fs_in.FragPosLightSpace); // is in shadow or not
+    float bias = max(0.05 * (1.0 - dot(normal, lightDir)), 0.005); // calculate the bias according to the slop
+    float shadow = ShadowCalculation(fs_in.FragPosLightSpace, bias); // is in shadow or not
     vec3 lighting = (ambient + (1.0 - shadow) * (diffuse + specular)) * color;
 
     FragColor = vec4(lighting, 1.0);
